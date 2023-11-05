85301206d8c1978fea27e698ac2e3fd9c2204e32
LolaNeh <loisekmokua@gmail.com>
2023-11-05 09:22:58 -0600

    updates on keypair

--- Keypair.tf
+++ Keypair.tf
@@ -1,11 +1,11 @@
-# Generates a secure private k ey and encodes it as PEM
+# Generates a secure private key and encodes it as PEM
 resource "tls_private_key" "ec2_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 # Create the Key Pair
 resource "aws_key_pair" "ec2_key" {
-  key_name   = "privatekeypair"  
+  key_name   = "privatekeypair1"  
   public_key = tls_private_key.ec2_key.public_key_openssh
 }
 # Save file
